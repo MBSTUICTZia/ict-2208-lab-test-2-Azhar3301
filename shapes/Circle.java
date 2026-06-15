69e2fba287cef322ba636e9db713ce298acbba31
Azhar3301 <it24031@mbstu.ac.bd>
2026-06-15 12:10:52 +0600

    Fix package declaration and update toString method

--- shapes/Circle.java
+++ shapes/Circle.java
@@ -1,11 +1,16 @@
 package shapes;
+
 public class Circle extends Shape {
+
     private double radius;
+
     public Circle(String color, boolean filled,
                   double radius) {
+
         super(color, filled);
         this.radius = radius;
     }
+
     @Override
     public double getArea() {
         return Math.PI * radius * radius;
@@ -15,14 +20,17 @@ public class Circle extends Shape {
     public double getPerimeter() {
         return 2 * Math.PI * radius;
     }
+
     @Override
     public String toString() {
-        return "[ Circle ]\n" +
+
+        return "[ shapes.Circle ]\n" +
                 super.toString() + "\n" +
                 "Radius: " + radius + "\n" +
                 String.format("Area         : %.2f", getArea()) + "\n" +
                 String.format("Circumference: %.2f", getPerimeter());
     }
+
     public double getRadius() {
         return radius;
     }
