f60ad56a646f6184e5fff0d19b255af73bf61fac
Azhar3301 <it24031@mbstu.ac.bd>
2026-06-15 12:06:12 +0600

    Update ShapeMain.java

--- shapes/ShapeMain.java
+++ shapes/ShapeMain.java
@@ -1,51 +1,40 @@
 package shapes;
+
 import java.util.Scanner;
+
 public class ShapeMain {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Shape[] shapes = new Shape[n];
-        for (int i=0;i<n;i++) {
-            String type = sc.next();
-            String color = sc.next();
-            boolean filled = sc.nextBoolean();
-            if (type.equals("RECTANGLE")) {
-                double width = sc.nextDouble();
-                double length = sc.nextDouble();
-                shapes[i] = new Rectangle(
-                        color,
-                        filled,
-                        width,
-                        length
-                );
-            } else if (type.equals("CIRCLE")) {
-                double radius = sc.nextDouble();
-                shapes[i] = new Circle(
-                        color,
-                        filled,
-                        radius
-                );
-            }
+
+        String type = sc.next();
+        String color = sc.next();
+        boolean filled = sc.nextBoolean();
+
+        Shape shape;
+
+        if (type.equalsIgnoreCase("rectangle")) {
+
+            double width = sc.nextDouble();
+            double length = sc.nextDouble();
+
+            shape = new Rectangle(color, filled, width, length);
+
+        } else if (type.equalsIgnoreCase("circle")) {
+
+            double radius = sc.nextDouble();
+
+            shape = new Circle(color, filled, radius);
+
+        } else {
+            System.out.println("Invalid shape");
+            sc.close();
+            return;
         }
-        for (Shape s : shapes) {
-            System.out.println(s);
-            System.out.println();
-        }
-        System.out.println("--- Downcast Check ---");
-        for (Shape s : shapes){
-            if (s instanceof Rectangle) {
-                Rectangle r = (Rectangle) s;
-                System.out.println(
-                        "Rectangle width=" + r.getWidth()
-                                + " length=" + r.getLength()
-                );
-            } else if (s instanceof Circle) {
-                Circle c = (Circle) s;
-                System.out.println(
-                        "Circle radius=" + c.getRadius()
-                );
-            }
-        }
+
+        System.out.println(shape);
+
         sc.close();
     }
 }
