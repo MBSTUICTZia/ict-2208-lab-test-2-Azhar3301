3295514bbb1a82c38f1895b24de50ac1899ac3bb
Azhar3301 <it24031@mbstu.ac.bd>
2026-06-15 12:10:31 +0600

    Update Rectangle.java

--- shapes/Rectangle.java
+++ shapes/Rectangle.java
@@ -1,7 +1,10 @@
 package shapes;
+
 public class Rectangle extends Shape {
+
     private double width;
     private double length;
+
     public Rectangle(String color, boolean filled,
                      double width, double length) {
 
@@ -9,26 +12,32 @@ public class Rectangle extends Shape {
         this.width = width;
         this.length = length;
     }
+
     @Override
     public double getArea() {
         return width * length;
     }
+
     @Override
     public double getPerimeter() {
         return 2 * (width + length);
     }
+
     @Override
     public String toString() {
-        return "[ Rectangle ]\n" +
+
+        return "[ shapes.Rectangle ]\n" +
                 super.toString() + "\n" +
                 "Width : " + width + "\n" +
                 "Length: " + length + "\n" +
                 String.format("Area      : %.2f", getArea()) + "\n" +
                 String.format("Perimeter : %.2f", getPerimeter());
     }
+
     public double getWidth() {
         return width;
     }
+
     public double getLength() {
         return length;
     }
